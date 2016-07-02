c18b65164b77b163ee2b6e2a376f688a1f63b70c
TranAnhVu <vuta@asiantech.vn>
2016-07-02 20:16:40 +0700

    Bootstrap angular app

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,9 @@
 'use strict';
 
 // Declare our `templateApp` and its depends.
+
+var myApp = angular.module('myApp', [
+	'ngRoute', // using for routing
+	'myControllers', // module contains all my controller
+]);
+
